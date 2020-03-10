b6f92d47e3d2bd8bf8f2f25df44486ebcc4b0eb1
LuckyPao <narumedsrpit@gmail.com>
2020-03-10 13:23:02 +0700

    Exercise4_Narumedsr_P.py
    Exercise4_Narumedsr_P.py Sent

---
+++ Exercise4_Narumedsr_P.py
@@ -0,0 +1,9 @@
+FE = 50
+GB = 60
+CS = 70
+CB = 80
+print("--- Your Score ---")
+print("Foundation English               : ", FE, " Score")
+print("General Business                 : ", GB, " Score")
+print("Introduction to Computer Systems : ", CS, " Score")
+print("Computer Programming             : ", CB, " Score")
